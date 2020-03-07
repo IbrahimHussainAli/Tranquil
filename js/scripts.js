bd2a5795f7f8cbf250a047c595fe617e1031946e
Ibrahim <ibs_7867@hotmail.com>
2020-03-07 17:23:16 +0000

    Redesign of the nav bar
    Re-coding the nav bar to ensure better understanding and better responsive ability

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("hello");
+
+function menuFunction() {
+  var x = document.getElementById("nav-menu");
+  if (x.className === "mainMenu") {
+    x.className += " responsive";
+  } else {
+    x.className = "mainMenu";
+  }
+}
