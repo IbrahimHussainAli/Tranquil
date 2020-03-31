fbda70607ca2e38f108e8e533162365786ebb5b2
Ibrahim <ibs_7867@hotmail.com>
2020-03-31 01:44:04 +0100

    add function for slideshow
    javascript functionality for homepage slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 console.log("hello");
 
+//NavigationBar
 let mainNav = document.getElementById('nav-menu');
 
 let navBarToggle = document.getElementById('js-navbar-toggle');
@@ -7,3 +8,20 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+//Homepage slide
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("featureSlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2500); // Change image every 2 seconds
+}
